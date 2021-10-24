2b519535ac22b6786d4e920136ac6d34eaceddea
emilelm <emil.elmarsson@gmail.com>
2021-10-24 21:16:36 +0200

    Refactoring; board logic into new module & cleanup

---
+++ src/logic.rs
@@ -0,0 +1,102 @@
+const EMPTY: u32 = 0;
+const BOARD_SIZE: usize = 4;
+
+use std::fmt;
+
+#[derive(PartialEq)]
+pub enum Dir{
+    Left,
+    Up,
+    Right,
+    Down
+}
+
+#[derive(Debug)]
+pub struct Board{
+    pub tiles: [[u32; BOARD_SIZE]; BOARD_SIZE],
+}
+
+impl Board{
+    fn set_column(&mut self, col_index: usize, column: &[u32; 4]){
+        for i in 0..BOARD_SIZE{
+            self.tiles[i][col_index] = column[i];
+        }
+    }
+
+    pub fn swipe(&mut self, dir: Dir){
+        for i in 0..BOARD_SIZE{
+            let mut new_row = [0u32; 4];
+            let mut counter = 0;
+            let mut merge = true;
+
+            for j in 0..BOARD_SIZE{
+                let cur_tile = 
+                    match dir{
+                        Dir::Left => self.tiles[i][j],
+                        Dir::Up => self.tiles[j][i],
+                        Dir::Right => self.tiles[i][BOARD_SIZE - 1 - j],
+                        Dir::Down => self.tiles[BOARD_SIZE - 1 - j][i],
+                    };
+
+                if cur_tile == EMPTY{
+                    continue;
+                }
+
+                if merge && counter > 0 && cur_tile == new_row[counter - 1]{
+                    new_row[counter - 1] *= 2;
+                    merge = false;
+                }else{
+                    new_row[counter] = cur_tile;
+                    merge = true;
+                    counter += 1;
+                }
+            }
+
+            match dir{
+                Dir::Left => {
+                    self.tiles[i] = new_row;
+                },
+                Dir::Up => {
+                    self.set_column(i, &new_row);
+                },
+                Dir::Right => {
+                    new_row.reverse();
+                    self.tiles[i] = new_row;
+                },
+                Dir::Down => {
+                    new_row.reverse();
+                    self.set_column(i, &new_row);
+                },
+            };
+        }
+    }
+
+    pub fn new() -> Board{
+        let mut tiles = [[0u32; BOARD_SIZE]; BOARD_SIZE];
+
+        tiles[0] = [2, 2, 4, 2];
+        tiles[1] = [4, 0, 0, 4];
+        tiles[2] = [2, 2, 2, 2];
+        tiles[3] = [0, 4, 2, 0];
+
+        Board { tiles: tiles }
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for i in 0..BOARD_SIZE{
+            for j in 0..BOARD_SIZE{
+                if self.tiles[i][j] == EMPTY{
+                    write!(f, "- ");
+                } else {
+                    write!(f, "{} ", self.tiles[i][j]);
+                }
+            }
+    
+            write!(f, "\n");
+        }
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,148 +1,15 @@
-use std::fmt;
+mod logic;
+
 use std::io;
 use std::io::Read;
 use termion::{async_stdin, event::Key, input::TermRead, raw::IntoRawMode};
 use tui::backend::TermionBackend;
 use tui::layout::{Constraint, Direction, Layout, Alignment};
 use tui::style::{Color, Style, Modifier};
-use tui::text::Spans;
 use tui::widgets::{Block, Borders, Paragraph};
 use tui::Terminal;
 
-#[derive(PartialEq)]
-enum Dir{
-    Left,
-    Up,
-    Right,
-    Down
-}
-
-const EMPTY: u32 = 0;
-const BOARD_SIZE: usize = 4;
-
-#[derive(Debug)]
-struct Board{
-    board: [[u32; BOARD_SIZE]; BOARD_SIZE],
-}
-
-impl Board{
-    const colors: [Color; 11] = [
-        Color::Rgb(238, 228, 218),
-        Color::Rgb(237, 224, 200),
-        Color::Rgb(242, 177, 121),
-        Color::Rgb(245, 149, 99),
-        Color::Rgb(246, 124, 95),
-        Color::Rgb(246, 94 ,59),
-        Color::Rgb(237, 207, 114),
-        Color::Rgb(237, 204, 97),
-        Color::Rgb(237, 200, 80),
-        Color::Rgb(237, 197, 63),
-        Color::Rgb(237, 194, 46),
-    ];
-
-    fn set_column(&mut self, col_index: usize, column: &[u32; 4]){
-        for i in 0..BOARD_SIZE{
-            self.board[i][col_index] = column[i];
-        }
-    }
-
-    fn swipe(&mut self, dir: Dir){
-        for i in 0..BOARD_SIZE{
-            let mut new_row = [0u32; 4];
-            let mut counter = 0;
-            let mut merge = true;
-
-            for j in 0..BOARD_SIZE{
-                let cur_tile = 
-                    match dir{
-                        Dir::Left => self.board[i][j],
-                        Dir::Up => self.board[j][i],
-                        Dir::Right => self.board[i][BOARD_SIZE - 1 - j],
-                        Dir::Down => self.board[BOARD_SIZE - 1 - j][i],
-                    };
-
-                if cur_tile == EMPTY{
-                    continue;
-                }
-
-                if merge && counter > 0 && cur_tile == new_row[counter - 1]{
-                    new_row[counter - 1] *= 2;
-                    merge = false;
-                }else{
-                    new_row[counter] = cur_tile;
-                    merge = true;
-                    counter += 1;
-                }
-            }
-
-            match dir{
-                Dir::Left => {
-                    self.board[i] = new_row;
-                },
-                Dir::Up => {
-                    self.set_column(i, &new_row);
-                },
-                Dir::Right => {
-                    new_row.reverse();
-                    self.board[i] = new_row;
-                },
-                Dir::Down => {
-                    new_row.reverse();
-                    self.set_column(i, &new_row);
-                },
-            };
-        }
-    }
-
-    fn new() -> Board{
-        let mut board = [[0u32; BOARD_SIZE]; BOARD_SIZE];
-
-        board[0] = [2, 2, 4, 2];
-        board[1] = [4, 0, 0, 4];
-        board[2] = [2, 2, 2, 2];
-        board[3] = [0, 4, 2, 0];
-
-        Board { board: board }
-    }
-}
-
-impl fmt::Display for Board {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for i in 0..BOARD_SIZE{
-            for j in 0..BOARD_SIZE{
-                if self.board[i][j] == EMPTY{
-                    write!(f, "- ");
-                } else {
-                    write!(f, "{} ", self.board[i][j]);
-                }
-            }
-    
-            write!(f, "\n");
-        }
-
-        Ok(())
-    }
-}
-
-/*fn main(){
-    let mut board = Board::new();
-
-    loop{
-        println!("{}", board);
-        
-        let mut line = String::new();
-        println!("Enter direction (U, R, D, L):");
-
-        std::io::stdin().read_line(&mut line).unwrap();
-        match line.to_uppercase().trim() {
-            "U" => {board.swipe(Direction::Up)},
-            "R" => {board.swipe(Direction::Right)},
-            "D" => {board.swipe(Direction::Down)},
-            "L" => {board.swipe(Direction::Left)},
-            _ => ()
-        }
-    }
-}*/
+use logic::{Board, Dir};
 
 fn main() -> Result<(), io::Error> {
     let mut board = Board::new();
@@ -185,7 +52,7 @@ fn main() -> Result<(), io::Error> {
                     .split(row_chunks[i]);
 
                 for j in 0..4{
-                    let tile = board.board[j][i];
+                    let tile = board.tiles[j][i];
                     let tile_string: String = if tile == 0 { String::new() } else { tile.to_string() };
                     let bg_color: Color = 
                         if tile == 2 { Color::Rgb(238, 228, 218) } 
